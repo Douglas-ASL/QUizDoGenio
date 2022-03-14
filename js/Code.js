b3dcbe6b097dc69f09214a13727ece8b932ac5ca
Douglas-ASL <94068133+Douglas-ASL@users.noreply.github.com>
2022-03-14 15:46:04 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Dica: Um conjunto de instruções que executa uma tarefa ou calcula um valor", 400,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Dica: Recolhe informações com a mesma velocidade que a Internet das Coisas é capaz de transmiti-las", 3,270)
+
+}
